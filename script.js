3f7ef54418147e7ccf7ad0d2f1ab561fbd5549bc
Armia Afridi <49521014+Armia13@users.noreply.github.com>
2019-09-03 06:20:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -101,21 +101,107 @@ function wendingMachine(money, numb) {
 // }
 // let myPhone = manufactureMobiles("Vivo Y71", "Android", "Oreo", "16GB", 5, "12-12-2017", ["6.0 inches screen", "Corning Gorilla Glass Protection",
 //   "13 MP Rear Camera", "No Fingerprint Sensor"])
+//homework
+ //mobile
+function manufactureMobiles(
+  name,
+  os,
+  version,
+  memory,
+  batteryTimings,
+  releaseDate,
+  features,
+  warranty
+) {
+  return {
+    name: name,
+    system: os,
+    ver: version,
+    mem: memory,
+    battery: batteryTimings,
+    date: releaseDate,
+    feat: features,
+    warr: warranty,
+    testBattery: function() {
+      console.log(`Battery timings is ${batteryTimings} hours`);
+    }
+  };
+}
+let myPhone = manufactureMobiles(
+  "Vivo Y71",
+  "Android",
+  "Oreo",
+  "16GB",
+  5,
+  "12-12-2017",
+  [
+    "6.0 inches screen",
+    "Corning Gorilla Glass Protection",
+    "13 MP Rear Camera",
+    "No Fingerprint Sensor"
+  ]
+);
 
-  function manufactureMobiles(name, os, version, memory, batteryTimings, releaseDate, features, warranty) {
-   return {
-      name: name,
-      system: os,
-      ver: version,
-      mem: memory,
-      battery: batteryTimings,
-      date: releaseDate,
-      feat: features,
-      warr: warranty,
-      testBattery: function () {
-        console.log(`Battery timings is ${batteryTimings} hours`)
+//Report
+function manufactureReport(
+  stdName,
+  stdClass,
+  stdSubj,
+  stdMarks,
+  stdRank,
+  stdPercentage,
+  stdGrade,
+  isPromoted
+) {
+  return {
+    name: stdName,
+    class: stdClass,
+    subjects: stdSubj,
+    marks: stdMarks,
+    rank: stdRank,
+    percentage: stdPercentage,
+    grade: stdGrade,
+    isPromoted: isPromoted,
+    showResult: function() {
+      if (this.isPromoted === true) {
+        console.log(`${this.name} has been promoted to Class 2`);
+      } else {
+        console.log(`${this.name} has been detained`);
       }
-    };
-  }
-  let myPhone = manufactureMobiles("Vivo Y71", "Android", "Oreo", "16GB", 5, "12-12-2017", ["6.0 inches screen", "Corning Gorilla Glass Protection",
-    "13 MP Rear Camera", "No Fingerprint Sensor"])
+    }
+  };
+}
+let myReport = manufactureReport(
+  "Ahmed",
+  1,
+  ["english", "urdu", "math"],
+  400,
+  "1st",
+  90,
+  "A+",
+  true
+);
+
+function manufactureShoes(name, company, size, color, type, gender, price) {
+  return {
+    name: name,
+    company: company,
+    size: size,
+    color: color,
+    type: type,
+    gender: gender,
+    price: price,
+    testRun: function() {
+      console.log(`${this.name} are comfortable shoes`);
+    }
+  };
+}
+let myShoes = manufactureShoes(
+  "Nike Joyride Run Flyknit",
+  "Nike",
+  [36, 38, 39],
+  "pastel pink",
+  "running shoes",
+  "female",
+  10000
+);
